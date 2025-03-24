c6f3c5f294df278f04d891f1dc21633d0bf0ed52
andrebbs <andre.bbs@gmail.com>
2025-03-24 14:05:22 -0300

    chore: update AWS RDS configuration and improve Terraform variables

--- rds.tf
+++ rds.tf
@@ -1,21 +1,24 @@
 resource "aws_db_instance" "postgres" {
   allocated_storage    = var.db_allocated_storage
   engine               = "postgres"
-  engine_version       = "14.5" # Substitua pela versão desejada
+  engine_version       = var.db_engine_version
   instance_class       = var.db_instance_class
-  name                 = var.db_name
+  identifier           = var.db_name
   username             = var.db_username
   password             = var.db_password
-  vpc_security_group_ids = var.vpc_security_group_ids
-  db_subnet_group_name = aws_db_subnet_group.postgres_subnet_group.name
+  parameter_group_name = var.db_parameter_group_name
   skip_final_snapshot  = true
-  publicly_accessible  = false
+
+  # Configuração de rede
+  publicly_accessible   = var.db_publicly_accessible
+  vpc_security_group_ids = [var.sgId] # Usando o Security Group existente
+  db_subnet_group_name   = aws_db_subnet_group.rds_subnet_group.name
 }
 
-resource "aws_db_subnet_group" "postgres_subnet_group" {
-  name       = "postgres-subnet-group"
-  subnet_ids = var.db_subnet_ids
-  tags = {
-    Name = "postgres-subnet-group"
-  }
-}
+# resource "aws_db_subnet_group" "postgres_subnet_group" {
+#   name       = "postgres-subnet-group"
+#   subnet_ids = var.db_subnet_ids
+#   tags = {
+#     Name = "postgres-subnet-group"
+#   }
+# }
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,12 +1,7 @@
 aws_region = "us-east-1"
-db_name = "fastfood_db"
+db_name = "fastfooddb"
 db_username = "admin"
 db_password = "securepassword"
 db_instance_class = "db.t3.micro"
 db_allocated_storage = 20
 vpc_security_group_ids = ["sg-0c52dcdb0a18aab05"]
-db_subnet_ids = [
-  "subnet-0da58b715c4447b40",
-  "subnet-0dc8c17af2d69e6c4",
-  "subnet-01e7d29bdec7e71dd"
-]
